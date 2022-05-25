7a48154449f862eae31f6027e34c040d433c5a4e
GonLudwig <glaucox.x@gmail.com>
2022-05-24 22:45:14 -0300

    Finalizacao de exercicios

--- 3 periodo/Laboratório de Ordenação e Pesquisa/RadixSort - 01-06/06/2.c
+++ 3 periodo/Laboratório de Ordenação e Pesquisa/RadixSort - 01-06/06/2.c
@@ -30,3 +30,55 @@ int preencherStrings(){
         printf("\n");
     }
 }
+
+// Juca
+
+#define MAX 30
+#define QTD 36
+
+int encontrarBalde( char letra ){
+  int numero = (int)letra;
+  
+  if( numero <= 57 ){
+    numero -= 48;
+  }
+  else{
+    numero -= 55;
+  }
+  return numero;
+}
+
+void radixSort(char** placas){
+  char balde[QTD], letra;
+  int contador[QTD], b, i, j, k, aux;
+
+  for(i=0; i<QTD; i++){
+    balde[i] = (char)malloc( MAX*sizeof(char*) );
+
+    for(j=0; j<MAX; j++){
+      balde[i][j] = (char*)malloc( 8*sizeof(char) );
+    }
+  }
+
+  for(i=6; i>=0; i--){
+    for(j=0; j<QTD; j++){
+      contador[j] = 0;
+    }
+
+    for(j=0; j<MAX; j++){
+      letra = placas[j][i];
+      b = encontrarBalde(letra);
+      strcpy( balde[b][contador[b]], placas[j] );
+      contador[b]++;
+    }
+
+    aux = 0;
+    for(j=0; j<QTD; j++){
+      if( contador[j] != 0 ){
+        for(k=0; k<contador[j]; k++){
+          strcpy( placas[aux++], balde[j][k] );
+        }
+      }
+    }
+  }
+}
---
+++ 3 periodo/Ordenação e Pesquisa/07 - Busca Binária e Sequencial - 07-06/2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void buscaSequencial(int *vetor, int finder) {
+    int i;
+
+    for (i = 0; i < count; i++)
+    {
+        
+    }
+    
+}
