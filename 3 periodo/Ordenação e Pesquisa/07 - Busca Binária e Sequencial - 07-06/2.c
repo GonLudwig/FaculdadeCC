ad85a9f63a3d504b246d4f339145f0f35630a35f
GonLudwig <64503923+GonLudwig@users.noreply.github.com>
2022-05-30 20:38:22 -0300

    aula 30-05

--- 3 periodo/Ordenação e Pesquisa/07 - Busca Binária e Sequencial - 07-06/2.c
+++ 3 periodo/Ordenação e Pesquisa/07 - Busca Binária e Sequencial - 07-06/2.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-void buscaSequencial(int *vetor, int finder) {
+void buscaSequencial(int *vetor, int busca, int tamanho) {
     int i;
 
-    for (i = 0; i < count; i++)
+    for (i = 0; i < tamanho; i++)
     {
-        
+        vetor[]
     }
     
 }
